53e43ed14362aeab2a51ab0411dbfcfe333faff2
Candelario Morales Ajcot <cdamorales595@gmail.com>
2016-05-25 12:01:08 -0600

    funcion personalizada branch cande

--- Hola.java
+++ Hola.java
@@ -2,5 +2,11 @@ public class Hola{
 
 	public static void main(String args[]){
 		System.out.println("Hola mundo");
+		String hola=holaPersonalizado();
+		System.out.println(hola);
+	}
+	
+	public String holaPersonalizado(){
+		return "hola cande!";
 	}
 }

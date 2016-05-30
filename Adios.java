9e9607225934a3bc59bc2b80144148db820b1dc0
SoftMusic <SoftMusic>
2016-05-30 11:29:16 -0600

    Procedimiento boleano

--- Adios.java
+++ Adios.java
@@ -1,5 +1,10 @@
 public class Adios{
 	public static void main(String args[]){
 		System.out.println("Adios");
+		System.out.println("Estado: "+boleano());
+	}
+	
+	public boolean boleano(){
+		return true;
 	}
 }

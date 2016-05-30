e89f3658790e3b9b1461411025e6b77ff84b5618
Mynor Barillas <mrbarillas19@gmail.com>
2016-05-30 13:21:15 -0600

    Nueva clase mynor 2

---
+++ Mynor2.java
@@ -0,0 +1,10 @@
+public class Mynor2{
+	public static void main(String args[]){
+		System.out.println("Clase Mynor 2");
+		Mynor2 obj = new Mynor2();
+		System.out.println("Suma: "+obj.sumar());
+	}
+	public int sumar(){
+		return 2+2;
+	}
+}

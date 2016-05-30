e4c398eb7cf38351981aa51dd47aadc2f19039db
Victor Orozco <vorozco@nabenik.com>
2016-05-30 11:27:28 -0600

    Se agrega un generador de saludos

--- Adios.java
+++ Adios.java
@@ -2,4 +2,8 @@ public class Adios{
 	public static void main(String args[]){
 		System.out.println("Adios");
 	}
+
+	public String generarSaludo(String nombre){
+		return "Hola ".concat(nombre);
+	}
 }

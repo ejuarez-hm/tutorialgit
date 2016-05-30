47075b339f117f572d10d7e04a90412fb8026783
Esteban Juarez <director.software@maristasac.org>
2016-05-30 11:27:59 -0600

    Cambio desde PC EJ

--- Adios.java
+++ Adios.java
@@ -1,5 +1,10 @@
 public class Adios{
 	public static void main(String args[]){
 		System.out.println("Adios");
+		imprime();
+	}
+	public void imprime(){
+		
+		System.out.println("Maquina esteban");
 	}
 }
